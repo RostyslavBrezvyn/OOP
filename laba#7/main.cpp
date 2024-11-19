c15466113cf0f438e9dca48e7cd27cb1e0b0eaf4
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-19 15:47:50 +0200

    Update main.cpp

--- laba#7/main.cpp
+++ laba#7/main.cpp
@@ -1,1 +1,33 @@
+#include "student.h"
+
+int main() {
+    Student group;
+
+    // Додавання студентів
+    group.addStudent(1, "Нестор Руслан", 1);
+    group.addStudent(2, "Брезвин Ростислав", 3);
+    group.addStudent(3, "Савчак Роман", 2);
+
+    // Виведення списку
+    std::cout << "\nПоточний список студентів:" << std::endl;
+    group.printStudents();
+
+    // Оновлення курсу студента
+    std::cout << "\nОновлення курсу студента з ID 2:" << std::endl;
+    group.updateStudentCourse(2, 4);
+
+    // Видалення студента
+    std::cout << "\nВидалення студента з ID 1:" << std::endl;
+    group.removeStudent(1);
+
+    // Виведення оновленого списку
+    std::cout << "\nОновлений список студентів:" << std::endl;
+    group.printStudents();
+
+    // Очищення списку студентів
+    std::cout << "\nОчищення списку студентів:" << std::endl;
+    group.clearStudents();
+
+    return 0;
+}
 
