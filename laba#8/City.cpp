18e56350c9eabd55f32fa8f1e7fbb32aace7f465
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:54:19 +0200

    Update City.cpp

--- laba#8/City.cpp
+++ laba#8/City.cpp
@@ -1,1 +1,22 @@
+#include "City.h"
+#include <iostream>
+
+City::City(string name, int population) : name(name), population(population) {}
+
+City::~City() {}
+
+void City::inputData() {
+    cout << "Enter city name: ";
+    cin >> name;
+    cout << "Enter population: ";
+    cin >> population;
+    if (population < 0) {
+        cout << "Population cannot be negative. Setting to 0." << endl;
+        population = 0;
+    }
+}
+
+void City::displayData() const {
+    cout << "City: " << name << ", Population: " << population << endl;
+}
 
