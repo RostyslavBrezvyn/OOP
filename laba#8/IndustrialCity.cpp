ccdee3a0da1c22f2d9cbb57dd49c21dc2c0bb110
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:55:11 +0200

    Update IndustrialCity.cpp

--- laba#8/IndustrialCity.cpp
+++ laba#8/IndustrialCity.cpp
@@ -1,1 +1,10 @@
+#include "IndustrialCity.h"
+#include <iostream>
+
+IndustrialCity::IndustrialCity(string name, int population) : City(name, population) {}
+
+void IndustrialCity::displayData() const {
+    cout << "Industrial City - ";
+    City::displayData();
+}
 
