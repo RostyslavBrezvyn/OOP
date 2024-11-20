f1f2c3d3893e5734c5ff2b44a1c57e67a190afd4
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 17:19:48 +0200

    Update CulturalCity.cpp

--- laba#9/CulturalCity.cpp
+++ laba#9/CulturalCity.cpp
@@ -1,1 +1,26 @@
+#include "CulturalCity.h"
+
+// Конструктор
+CulturalCity::CulturalCity(const std::string& name, int population, int museums) 
+    : City(name, population), museums(museums) {}
+
+// Деструктор
+CulturalCity::~CulturalCity() {}
+
+// Виведення даних
+void CulturalCity::displayData() const {
+    City::displayData();
+    std::cout << "Кількість музеїв: " << museums << std::endl;
+}
+
+// Введення даних
+void CulturalCity::inputData() {
+    City::inputData();
+    std::cout << "Введіть кількість музеїв: ";
+    std::cin >> museums;
+}
+
+// Методи
+int CulturalCity::getMuseums() const { return museums; }
+void CulturalCity::setMuseums(int museums) { this->museums = museums; }
 
