b750626c6ec73459aaeea1daf874f313b5bc75ef
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 17:19:38 +0200

    Update CulturalCity.h

--- laba#9/CulturalCity.h
+++ laba#9/CulturalCity.h
@@ -1,1 +1,23 @@
+#ifndef CULTURALCITY_H
+#define CULTURALCITY_H
+
+#include "City.h"
+
+class CulturalCity : public City {
+private:
+    int museums;  // Кількість музеїв
+
+public:
+    CulturalCity(const std::string& name = "", int population = 0, int museums = 0);
+    ~CulturalCity();
+
+    void displayData() const override; // Перевизначений метод
+    void inputData() override;         // Перевизначений метод
+
+    // Додаткові методи
+    int getMuseums() const;
+    void setMuseums(int museums);
+};
+
+#endif
 
