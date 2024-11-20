39b1680d099163b49b612fc8b15be8df59c960c6
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 17:19:22 +0200

    Update IndustrialCity.cpp

--- laba#9/IndustrialCity.cpp
+++ laba#9/IndustrialCity.cpp
@@ -1,1 +1,26 @@
+#include "IndustrialCity.h"
+
+// Конструктор
+IndustrialCity::IndustrialCity(const std::string& name, int population, int factories) 
+    : City(name, population), factories(factories) {}
+
+// Деструктор
+IndustrialCity::~IndustrialCity() {}
+
+// Виведення даних
+void IndustrialCity::displayData() const {
+    City::displayData();
+    std::cout << "Кількість заводів: " << factories << std::endl;
+}
+
+// Введення даних
+void IndustrialCity::inputData() {
+    City::inputData();
+    std::cout << "Введіть кількість заводів: ";
+    std::cin >> factories;
+}
+
+// Методи
+int IndustrialCity::getFactories() const { return factories; }
+void IndustrialCity::setFactories(int factories) { this->factories = factories; }
 
