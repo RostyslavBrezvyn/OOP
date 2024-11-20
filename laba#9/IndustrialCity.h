e010b77ed55142ca1add899722688ac357ab69ba
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 17:19:13 +0200

    Update IndustrialCity.h

--- laba#9/IndustrialCity.h
+++ laba#9/IndustrialCity.h
@@ -1,1 +1,23 @@
+#ifndef INDUSTRIALCITY_H
+#define INDUSTRIALCITY_H
+
+#include "City.h"
+
+class IndustrialCity : public City {
+private:
+    int factories;  // Кількість заводів
+
+public:
+    IndustrialCity(const std::string& name = "", int population = 0, int factories = 0);
+    ~IndustrialCity();
+
+    void displayData() const override; // Перевизначений метод
+    void inputData() override;         // Перевизначений метод
+
+    // Додаткові методи
+    int getFactories() const;
+    void setFactories(int factories);
+};
+
+#endif
 
