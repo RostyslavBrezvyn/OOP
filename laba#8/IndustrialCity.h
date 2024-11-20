247aeb99517dff0988e844478b3a75eca2a31b92
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:55:00 +0200

    Update IndustrialCity.h

--- laba#8/IndustrialCity.h
+++ laba#8/IndustrialCity.h
@@ -1,1 +1,13 @@
+#ifndef INDUSTRIALCITY_H
+#define INDUSTRIALCITY_H
+
+#include "City.h"
+
+class IndustrialCity : public City {
+public:
+    IndustrialCity(string name, int population);
+    void displayData() const override;
+};
+
+#endif
 
