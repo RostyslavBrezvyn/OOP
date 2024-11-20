4ddcd4c6759e453e8d049e830a276176a7ec6af1
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:54:47 +0200

    Update CulturalCity.cpp

--- laba#8/CulturalCity.cpp
+++ laba#8/CulturalCity.cpp
@@ -1,1 +1,10 @@
+#include "CulturalCity.h"
+#include <iostream>
+
+CulturalCity::CulturalCity(string name, int population) : City(name, population) {}
+
+void CulturalCity::displayData() const {
+    cout << "Cultural City - ";
+    City::displayData();
+}
 
