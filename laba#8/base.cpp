eb95cc86c9093c33d4e03be9f2ea699004956c32
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:11:55 +0200

    Update base.cpp

--- laba#8/base.cpp
+++ laba#8/base.cpp
@@ -1,1 +1,18 @@
+#include "Base.h"
+
+Base::Base() : studentName("") {}
+
+Base::Base(const string& name) : studentName(name) {}
+
+void Base::setStudentName(const string& name) {
+    studentName = name;
+}
+
+string Base::getStudentName() const {
+    return studentName;
+}
+
+void Base::display() const {
+    cout << "Student Name: " << studentName << endl;
+}
 
