13f25667d3318ce7b66bf864a1f2ce36e22d1b04
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:52:29 +0200

    Update main.cpp

--- laba#8/main.cpp
+++ laba#8/main.cpp
@@ -1,46 +1,21 @@
 #include <iostream>
 #include <vector>
-#include "Exam.h"
-
-using namespace std;
+#include "IndustrialCity.h"
+#include "CulturalCity.h"
 
 int main() {
-    vector<Exam> exams; // Динамічний масив для збереження об'єктів
-    int n;
+    vector<City*> cities;
 
-    cout << "Enter the number of students: ";
-    cin >> n;
+    cities.push_back(new IndustrialCity("Kharkiv", 1500000));
+    cities.push_back(new CulturalCity("Lviv", 800000));
 
-    for (int i = 0; i < n; ++i) {
-        string name;
-        int ticket;
-        double grade;
-
-        cout << "Enter student name: ";
-        cin >> ws;
-        getline(cin, name);
-
-        cout << "Enter ticket number: ";
-        cin >> ticket;
-
-        cout << "Enter grade: ";
-        cin >> grade;
-
-        try {
-            Exam exam(name, ticket, grade);
-            exams.push_back(exam);
-        } catch (const exception& e) {
-            cerr << "Error: " << e.what() << endl;
-            --i; // Повторне введення для цього студента
-        }
+    for (City* city : cities) {
+        city->displayData();
     }
 
-    cout << "\nStudent Data:\n";
-    for (const auto& exam : exams) {
-        exam.display();
-        cout << "----------------------\n";
+    for (City* city : cities) {
+        delete city;
     }
 
     return 0;
 }
-
