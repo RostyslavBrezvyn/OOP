b8c1161c63b3e18b53d24bf7bbcbad0b7bea0331
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:12:15 +0200

    Update exam.h

--- laba#8/exam.h
+++ laba#8/exam.h
@@ -1,1 +1,22 @@
+#ifndef EXAM_H
+#define EXAM_H
+
+#include "Base.h"
+
+class Exam : public Base {
+private:
+    int ticketNumber;
+    double grade;
+
+public:
+    Exam();
+    Exam(const string& name, int ticket, double grade);
+    void setTicketNumber(int ticket);
+    void setGrade(double grade);
+    int getTicketNumber() const;
+    double getGrade() const;
+    void display() const override;
+};
+
+#endif
 
