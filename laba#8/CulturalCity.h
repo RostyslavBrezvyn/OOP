6e5a4c2d5030f2340c1af40fab5d87374fb80b40
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:54:33 +0200

    Update CulturalCity.h

--- laba#8/CulturalCity.h
+++ laba#8/CulturalCity.h
@@ -1,1 +1,13 @@
+#ifndef CULTURALCITY_H
+#define CULTURALCITY_H
+
+#include "City.h"
+
+class CulturalCity : public City {
+public:
+    CulturalCity(string name, int population);
+    void displayData() const override;
+};
+
+#endif
 
