cfde8748e7233dc5b79ce51d0c4a40405e49b045
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:11:43 +0200

    Update base.h

--- laba#8/base.h
+++ laba#8/base.h
@@ -1,1 +1,21 @@
+#ifndef BASE_H
+#define BASE_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Base {
+protected:
+    string studentName;
+
+public:
+    Base();
+    Base(const string& name);
+    void setStudentName(const string& name);
+    string getStudentName() const;
+    virtual void display() const; // Віртуальний метод для виводу
+    virtual ~Base() = default;
+};
+
+#endif
 
