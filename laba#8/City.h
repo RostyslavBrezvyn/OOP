5e98f0227e54d5524f716e6887fb00526568a3a4
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 16:54:08 +0200

    Update City.h

--- laba#8/City.h
+++ laba#8/City.h
@@ -1,1 +1,21 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <string>
+using namespace std;
+
+class City {
+public:
+    City(string name, int population);
+    virtual ~City();
+    
+    virtual void inputData();
+    virtual void displayData() const;
+    
+protected:
+    string name;
+    int population;
+};
+
+#endif
 
