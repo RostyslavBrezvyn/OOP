3b6695df468a79e85b602bf33d42062f34e4dd5c
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-20 17:18:48 +0200

    Update City.h

--- laba#9/City.h
+++ laba#9/City.h
@@ -1,1 +1,29 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <string>
+#include <iostream>
+
+class City {
+protected:
+    std::string name;
+    int population;
+
+public:
+    City(const std::string& name = "", int population = 0);
+    virtual ~City();
+
+    virtual void displayData() const;  // Віртуальний метод для виведення даних
+    virtual void inputData();         // Віртуальний метод для введення даних
+
+    // Гетери
+    std::string getName() const;
+    int getPopulation() const;
+
+    // Сетери
+    void setName(const std::string& name);
+    void setPopulation(int population);
+};
+
+#endif
 
