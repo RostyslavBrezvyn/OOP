e9e803b42c1851b3d6b5f3e8713f146aa384ce09
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-11-27 18:27:47 +0200

    Update City.cpp

--- laba#8/City.cpp
+++ laba#8/City.cpp
@@ -6,17 +6,16 @@ City::City(string name, int population) : name(name), population(population) {}
 City::~City() {}
 
 void City::inputData() {
-    cout << "Enter city name: ";
+    cout << "Введіть назву міста: ";
     cin >> name;
-    cout << "Enter population: ";
+    cout << "Введіть населення: ";
     cin >> population;
     if (population < 0) {
-        cout << "Population cannot be negative. Setting to 0." << endl;
+        cout << "Населення не може бути від'ємним. Встановлено значення 0." << endl;
         population = 0;
     }
 }
 
 void City::displayData() const {
-    cout << "City: " << name << ", Population: " << population << endl;
+    cout << "Місто: " << name << ", Населення: " << population << endl;
 }
-
