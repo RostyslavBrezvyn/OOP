47d566e2ea3ebd781b7532e33ac3b1222b6c7911
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:12:23 +0200

    Update time.h

--- laba10/time.h
+++ laba10/time.h
@@ -1,1 +1,23 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+class Time {
+private:
+    int hours;
+    int minutes;
+
+public:
+    Time(int h = 0, int m = 0);
+
+    bool operator>=(const Time& other) const;
+    Time& operator+=(int minutesToAdd);
+    void display() const;
+    int differenceFromCurrentTime() const;
+};
+
+#endif // TIME_H
 
