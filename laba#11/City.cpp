6cfc8de467d171e789e54f8997655c1e3179d992
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:24:48 +0200

    Update City.cpp

--- laba#11/City.cpp
+++ laba#11/City.cpp
@@ -1,1 +1,19 @@
+#include "City.h"
+#include <iostream>
+using namespace std;
+
+City::City(const std::string& n, Address addr) : name(n), address(addr) {}
+
+void City::display() const {
+    cout << "Місто: " << name << ", Адреса: " << address.street << " " << address.number << endl;
+}
+
+void City::input() {
+    cout << "Введіть назву міста: ";
+    cin >> name;
+    cout << "Введіть вулицю: ";
+    cin >> address.street;
+    cout << "Введіть номер будинку: ";
+    cin >> address.number;
+}
 
