9cf30fc62bb8b6bb9330388cadbe245d3740dec2
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:25:18 +0200

    Update IndustrialCenter.cpp

--- laba#11/IndustrialCenter.cpp
+++ laba#11/IndustrialCenter.cpp
@@ -1,1 +1,18 @@
+#include "IndustrialCenter.h"
+#include <iostream>
+using namespace std;
+
+IndustrialCenter::IndustrialCenter(const std::string& n, Address addr, const std::string& type) 
+    : City(n, addr), industryType(type) {}
+
+void IndustrialCenter::display() const {
+    cout << "Промисловий центр: " << name << ", Тип промисловості: " << industryType
+         << ", Адреса: " << address.street << " " << address.number << endl;
+}
+
+void IndustrialCenter::input() {
+    City::input();
+    cout << "Введіть тип промисловості: ";
+    cin >> industryType;
+}
 
