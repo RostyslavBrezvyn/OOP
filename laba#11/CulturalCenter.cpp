0c9e1e66a14789467ebbf2e32ce10117ece4ee71
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:25:48 +0200

    Update CulturalCenter.cpp

--- laba#11/CulturalCenter.cpp
+++ laba#11/CulturalCenter.cpp
@@ -1,1 +1,18 @@
+#include "CulturalCenter.h"
+#include <iostream>
+using namespace std;
+
+CulturalCenter::CulturalCenter(const std::string& n, Address addr, const std::string& type) 
+    : City(n, addr), culturalType(type) {}
+
+void CulturalCenter::display() const {
+    cout << "Культурний центр: " << name << ", Тип культури: " << culturalType
+         << ", Адреса: " << address.street << " " << address.number << endl;
+}
+
+void CulturalCenter::input() {
+    City::input();
+    cout << "Введіть тип культури: ";
+    cin >> culturalType;
+}
 
