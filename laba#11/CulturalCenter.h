56d1552524fb193628e8fe2b58fdc41c9ab4d210
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:26:00 +0200

    Update CulturalCenter.h

--- laba#11/CulturalCenter.h
+++ laba#11/CulturalCenter.h
@@ -1,1 +1,17 @@
+#ifndef CULTURALCENTER_H
+#define CULTURALCENTER_H
+
+#include "City.h"
+
+class CulturalCenter : public City {
+private:
+    std::string culturalType;
+
+public:
+    CulturalCenter(const std::string& n, Address addr, const std::string& type);
+    void display() const override;
+    void input() override;
+};
+
+#endif
 
