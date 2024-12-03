d687bf6b24d002a00e15127c600acea775a69db0
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:25:30 +0200

    Update IndustrialCenter.h

--- laba#11/IndustrialCenter.h
+++ laba#11/IndustrialCenter.h
@@ -1,1 +1,17 @@
+#ifndef INDUSTRIALCENTER_H
+#define INDUSTRIALCENTER_H
+
+#include "City.h"
+
+class IndustrialCenter : public City {
+private:
+    std::string industryType;
+
+public:
+    IndustrialCenter(const std::string& n, Address addr, const std::string& type);
+    void display() const override;
+    void input() override;
+};
+
+#endif
 
