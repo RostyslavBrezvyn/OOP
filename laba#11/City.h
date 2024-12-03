7d38a90a2c582056dfec2f18fd664a5ba5be7952
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:24:58 +0200

    Update City.h

--- laba#11/City.h
+++ laba#11/City.h
@@ -1,1 +1,20 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <string>
+#include "Address.h"
+
+class City {
+protected:
+    std::string name;
+    Address address;
+
+public:
+    City(const std::string& n, Address addr);
+    virtual ~City() {}
+    virtual void display() const;
+    virtual void input();
+};
+
+#endif
 
