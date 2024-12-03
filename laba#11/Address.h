b1a7cb550adcf84a5d0a951ddeeb6908a80cca95
RostyslavBrezvyn <165325970+RostyslavBrezvyn@users.noreply.github.com>
2024-12-03 17:24:35 +0200

    Update Address.h

--- laba#11/Address.h
+++ laba#11/Address.h
@@ -1,1 +1,14 @@
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+#include <string>
+
+struct Address {
+    std::string street;
+    int number;
+
+    Address(const std::string& s = "", int n = 0) : street(s), number(n) {}
+};
+
+#endif
 
